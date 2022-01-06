c19e81b52a79b2c332f7fb0bb77900ef755517bf
heynar-max <heynar76@hotmail.com>
2022-01-05 20:04:19 -0500

    iniciando portafolio 1

---
+++ src/pages/Portafolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PortafolioCom from '../Components/PortafolioCom'
+
+const Portafolio = () => {
+    return (
+        <>
+            <PortafolioCom/>
+        </>
+    )
+}
+
+export default Portafolio
