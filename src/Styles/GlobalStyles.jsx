20693f2c70e0f11515a145a39a8a6ecb5d898a91
heynar-max <heynar76@hotmail.com>
2022-01-07 20:44:13 -0500

    About

--- src/Styles/GlobalStyles.jsx
+++ src/Styles/GlobalStyles.jsx
@@ -1,9 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+:root{
+    --fondo-rojo: #BD4451;
+    --negro: #000;
+    --blanco: #fff;
+
+}
 *{
     padding: 0;
     margin: 0;
+}
+body{
+    /* background-color: var(--negro); */
 }
 `;
 
